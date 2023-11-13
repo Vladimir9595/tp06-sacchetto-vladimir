cf178109b0baafde8c8f479357a5155476fe8aff
Vladimir SACCHETTO <vladimir.sacchetto@al-enterprise.com>
2023-11-13 15:10:25 +0100

    fix: add cart state

---
+++ src/app/shared/states/cart.state.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { State, Action, StateContext, Selector } from '@ngxs/store';
+import { AddToCart, RemoveFromCart } from '../actions/cart.actions';
+import { Product } from '../models/product';
+
+export interface CartStateModel {
+  items: Product[];
+}
+
+@State<CartStateModel>({
+  name: 'cart',
+  defaults: {
+    items: [],
+  },
+})
+@Injectable()
+export class CartState {
+  @Selector()
+  static cartItems(state: CartStateModel) {
+    return state.items;
+  }
+
+  @Selector()
+  static cartItemCount(state: CartStateModel) {
+    return state.items.length;
+  }
+
+  @Action(AddToCart)
+  addToCart(ctx: StateContext<CartStateModel>, action: AddToCart) {
+    const state = ctx.getState();
+    ctx.patchState({
+      items: [...state.items, action.payload],
+    });
+  }
+
+  @Action(RemoveFromCart)
+  removeFromCart(ctx: StateContext<CartStateModel>, action: RemoveFromCart) {
+    const state = ctx.getState();
+    const updatedItems = state.items.filter(
+      (item) => item.id !== action.payload
+    );
+    ctx.patchState({
+      items: updatedItems,
+    });
+  }
+}
