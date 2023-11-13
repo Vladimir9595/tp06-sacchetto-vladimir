b0dad4564e33a254c1b9d3f8935d9ed891251615
Vladimir SACCHETTO <vladimir.sacchetto@al-enterprise.com>
2023-11-13 23:26:30 +0100

    fix: add cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { CartState } from '../../shared/states/cart.state';
+import {
+  RemoveFromCart,
+  ClearCart,
+} from '../../shared/actions/cart.actions';
+import { Product } from '../../shared/models/product';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  @Select(CartState.cartItems) cartItems$: Observable<Product[]> | undefined;
 
+  constructor(private store: Store) {}
+
+  removeFromCart(productId: number) {
+    this.store.dispatch(new RemoveFromCart(productId));
+  }
+
+  clearCart() {
+    this.store.dispatch(new ClearCart());
+  }
 }
