5f62ea453f12179030c9c4a043cb81809bf19ac8
Vladimir SACCHETTO <vladimir.sacchetto@al-enterprise.com>
2023-11-13 14:01:29 +0100

    fix: update name project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tp03SacchettoVladimir
+# Tp04SacchettoVladimir
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.7.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tp03-sacchetto-vladimir';
+  title = 'tp04-sacchetto-vladimir';
 }
