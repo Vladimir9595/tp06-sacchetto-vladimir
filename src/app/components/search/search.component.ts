0ec6fb7b8977542812acd64ac3ecf5b3b0d3efdd
Vladimir SACCHETTO <vladimir.sacchetto@al-enterprise.com>
2023-11-13 15:14:29 +0100

    fix: add composent folder

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { ProductService } from '../services/product.service';
-import { Product } from '../shared/models/product';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../shared/models/product';
 
 @Component({
   selector: 'app-search',
