052b82c96d825a226acbcdadd94ab903d81b92ea
Vladimir SACCHETTO <vladimir.sacchetto@al-enterprise.com>
2023-11-13 16:00:18 +0100

    fix: add cart and routes in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { CartState } from '../../shared/states/cart.state';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Select(CartState.cartItemCount) cartItemCount$:
+    | Observable<number>
+    | undefined;
+
+  constructor() {}
+}
