065f30f251542f79443914f7a2967f6697675727
Vladimir9595 <vladisac@hotmail.it>
2023-11-23 21:42:52 +0100

    feat : add environment to backend

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  backendLoginClient: 'https://siweb-login.onrender.com/api/utilisateur/login',
+  backendCatalogue: 'https://siweb-login.onrender.com/api/catalogue',
+};
