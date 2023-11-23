0f84f96b5d39d8071af07b94180b67c75552b1ab
Vladimir9595 <vladisac@hotmail.it>
2023-11-23 21:44:40 +0100

    feat : modify product model class

--- src/app/shared/models/product.ts
+++ src/app/shared/models/product.ts
@@ -1,7 +1,15 @@
-export interface Product {
+export class Product {
   id: number;
   name: string;
   description: string;
   price: number;
   category: string;
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.description = '';
+    this.price = 0;
+    this.category = '';
+  }
 }
