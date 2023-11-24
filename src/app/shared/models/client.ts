bc500a9827f80d490bb885fdca86350c834f10f6
Vladimir9595 <vladisac@hotmail.it>
2023-11-24 14:40:14 +0100

    fix : modify lastName and firstName

--- src/app/shared/models/client.ts
+++ src/app/shared/models/client.ts
@@ -1,9 +1,9 @@
 export class Client {
-  username: string;
-  password: string;
+  firstName: string;
+  lastName: string;
 
-  constructor(username: string, password: string) {
-    this.username = username;
-    this.password = password;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
 }
