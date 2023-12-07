fa277955b34316a8f7ea24e5a205203ced06f4e6
Vladimir9595 <vladisac@hotmail.it>
2023-12-07 14:57:54 +0100

    tp06

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tp05SacchettoVladimir
+# Tp06SacchettoVladimir
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.7.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tp05-sacchetto-vladimir';
+  title = 'tp06-sacchetto-vladimir';
 }
