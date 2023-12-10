f86e60ce4fecc171004679d318f1905bf2bde126
Vladimir9595 <vladisac@hotmail.it>
2023-12-10 18:21:23 +0100

    feat : update filter method for backend

--- backend/api/controller.php
+++ backend/api/controller.php
@@ -22,9 +22,19 @@ function hello(Request $request, Response $response, $args)
 
 function  getSearchCalatogue(Request $request, Response $response, $args)
 {
-    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+    $filtre = $args['filtre'];
+    $flux = '[{"id":1,"name":"Chaussures de course","description":"C\'est la description du produit 1","price":19.99,"category":"Categorie 1"},{"id":2,"name":"Balle de foot","description":"C\'est la description du produit 2","price":9.99,"category":"Categorie 1"},{"id":3,"name":"Casque vélo","description":"C\'est la description du produit 3","price":7.99,"category":"Categorie 1"},{"id":4,"name":"Chaussettes de foot","description":"C\'est la description du produit 4","price":5.99,"category":"Categorie 1"},{"id":5,"name":"Vin rouge de Bourgogne","description":"C\'est la description du produit 5","price":25.99,"category":"Categorie 3"},{"id":6,"name":"Foie gras","description":"C\'est la description du produit 6","price":39.99,"category":"Categorie 3"},{"id":7,"name":"Montre-bracelet connectée","description":"C\'est la description du produit 7","price":49.99,"category":"Categorie 2"},{"id":8,"name":"Machine à café","description":"C\'est la description du produit 8","price":89.99,"category":"Categorie 2"},{"id":9,"name":"Enceinte Bluetooth","description":"C\'est la description du produit 9","price":13.99,"category":"Categorie 2"},{"id":10,"name":"Sac à dos","description":"C\'est la description du produit 10","price":18.99,"category":"Categorie 1"},{"id":11,"name":"Tablette tactile","description":"C\'est la description du produit 11","price":99.99,"category":"Categorie 2"},{"id":12,"name":"Crémant d\'Alsace","description":"C\'est la description du produit 12","price":37.99,"category":"Categorie 3"},{"id":13,"name":"Écran d\'ordinateur ViewScreen","description":"C\'est la description du produit 13","price":59.99,"category":"Categorie 2"},{"id":14,"name":"Téléphone portable TechMaster","description":"C\'est la description du produit 14","price":79.99,"category":"Categorie 2"},{"id":15,"name":"Dindon de saison","description":"C\'est la description du produit 15","price":25.99,"category":"Categorie 3"},{"id":16,"name":"Appareil photo numérique","description":"C\'est la description du produit 16","price":35.99,"category":"Categorie 2"},{"id":17,"name":"Gewurztraminer vendanges tardives d\'Alsace","description":"C\'est la description du produit 17","price":27.99,"category":"Categorie 3"},{"id":18,"name":"Console de jeux","description":"C\'est la description du produit 18","price":299.99,"category":"Categorie 2"},{"id":19,"name":"Casque audio sans fil","description":"C\'est la description du produit 19","price":31.99,"category":"Categorie 2"},{"id":20,"name":"Blouson en cuir","description":"C\'est la description du produit 20","price":48.99,"category":"Categorie 1"}]';
 
-    $response->getBody()->write($flux);
+    if ($filtre) {
+        $data = json_decode($flux, true);
+
+        $res = array_filter($data, function ($obj) use ($filtre) {
+            return stripos(strtolower($obj["name"]), $filtre) !== false || stripos(strtolower($obj["price"]), $filtre) !== false || stripos(strtolower($obj["category"]), $filtre) !== false;
+        });
+        $response->getBody()->write(json_encode(array_values($res)));
+    } else {
+        $response->getBody()->write($flux);
+    }
 
     return addHeaders($response);
 }
