873e2a3d2ba137da997289ba7433c25ae38e5545
Vladimir9595 <vladisac@hotmail.it>
2023-12-10 18:21:43 +0100

    feat : add filter products

--- src/app/components/products/catalog/catalog.component.ts
+++ src/app/components/products/catalog/catalog.component.ts
@@ -1,5 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { Observable, fromEvent, of } from 'rxjs';
+import {
+  map,
+  distinctUntilChanged,
+  debounceTime,
+  switchMap,
+  catchError,
+} from 'rxjs/operators';
 import { ProductService } from '../../../services/product.service';
 import { Product } from '../../../shared/models/product';
 import { Store } from '@ngxs/store';
@@ -12,12 +26,17 @@ import { AddToCart } from '../../../shared/actions/cart.actions';
   providers: [ProductService],
 })
 export class CatalogComponent implements OnInit {
-  searchName: string = '';
-  searchCategory: string = '';
-  searchPrice: number = 0;
   products$: Observable<Product[]>;
+  model?: Observable<any>;
+  searchField$?: Observable<any>;
+  @ViewChild('input', { static: true }) input!: ElementRef;
+  @Output() searchEvent = new EventEmitter<string>();
 
-  constructor(private productService: ProductService, private store: Store) {
+  constructor(
+    private productService: ProductService,
+    private store: Store,
+    private _service: ProductService
+  ) {
     this.products$ = this.productService.getProducts();
   }
 
@@ -25,5 +44,26 @@ export class CatalogComponent implements OnInit {
 
   addToCart(product: Product) {
     this.store.dispatch(new AddToCart(product));
+  }
+
+  ngAfterViewInit(): void {
+    this.searchField$ = fromEvent(this.input.nativeElement, 'keyup').pipe(
+      map((event: any) => event.target.value),
+
+      distinctUntilChanged(),
+      debounceTime(300),
+      switchMap((searchTerm: string) =>
+        this._service.searchCatalog(searchTerm).pipe(
+          catchError((error) => {
+            console.error('Erreur lors de la recherche :', error);
+            return of([]);
+          })
+        )
+      )
+    );
+
+    this.searchField$.subscribe((searchTerm) => {
+      this.searchEvent.emit(searchTerm);
+    });
   }
 }
